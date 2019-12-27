e9cabf67aed1270bdc62a2c94821f0dfab0042ce
diogonegrao <ruidiogopereira@gmail.com>
2019-12-27 16:36:35 +0000

    Layouts completos

---
+++ app/src/main/java/com/example/logistraffic_gestao/RegistarCargaActivity.java
@@ -0,0 +1,14 @@
+package com.example.logistraffic_gestao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistarCargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registar_carga);
+    }
+}
